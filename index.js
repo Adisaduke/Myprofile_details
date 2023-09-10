ab341ca3edbf9ba2c5b87b7f4a123a3b03d43072
Adisa Rasak <98124721+Adisaduke@users.noreply.github.com>
2023-09-10 14:57:51 +0100

    Update index.js
    Milliseconds

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ let month = date.getMonth() + 1;
 let year = date.getFullYear()
 let currentDate = `${day} - ${month} - ${year}`
 const date_display = document.querySelector('[data-testid="currentDayOfTheWeek"]')
-date_display.innerHTML = `Date: ${currentDate}`
+date_display.innerHTML = currentDate
 
 
 
 // code for time
-const time = Date.now()
+const time = new Date().getTime();
 const date_time = document.querySelector('[data-testid="currentUTCTime"]')
 date_time.innerHTML = time 
