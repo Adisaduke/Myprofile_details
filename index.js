8ca50cd69ccd8fc427e12445800535404c2c31b3
Adisa Rasak <98124721+Adisaduke@users.noreply.github.com>
2023-09-10 17:44:40 +0100

    Update index.js
    Exact dat

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 // code for date
-const date = new Date()
-
-let day = date.getDate();
-let month = date.getMonth() + 1;
-let year = date.getFullYear()
-let currentDate = `${day} - ${month} - ${year}`
+const currentDate = new Date()
+const options = { weekday: 'long'};
+const currentDay = currentDate.toLocaleDateString(undefined, options);
 const date_display = document.querySelector('[data-testid="currentDayOfTheWeek"]')
-date_display.innerHTML = currentDate
+date_display.innerHTML = currentDay
 
 
 
